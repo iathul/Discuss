dfc019042d82f83f33143ec03725316e58f16bbd
Athul Joy <aathuljjoy333@gmail.com>
2020-01-05 15:10:46 +0530

    added responsive

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ io.on('connection', (socket)=>{
     
 })
 
-http.listen(process.env.PORT || 3000,()=>{
-    console.log(" Server running at port 3000........!"); 
+http.listen(process.env.PORT || 8080,()=>{
+    console.log(" Server running at port 8080........!"); 
 })
  
